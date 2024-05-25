0c6000aa57fbe8fb8c20d00e71b7b961e879504b
YesianRohn <xsye20@fudan.edu.cn>
2024-05-25 11:27:48 +0800

    upload pj1 and pjj2

---
+++ exp_2/data.py
@@ -0,0 +1,279 @@
+from __future__ import print_function
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import os
+import nltk
+from PIL import Image
+import numpy as np
+from tqdm import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class FlickrDataset(data.Dataset):
+    """
+    Dataset loader for Flickr30k and Flickr8k full datasets.
+    """
+
+    def __init__(self, root, split, vocab, transform=None):
+        self.root = root
+        self.vocab = vocab
+        self.split = split
+        self.transform = transform
+        with open(os.path.join(root, '%s.txt' % split)) as f:
+            self.lines = f.readlines()
+
+    def __getitem__(self, index):
+        """This function returns a tuple that is further passed to collate_fn
+        """
+        vocab = self.vocab
+        img_id = index//5
+        root = self.root
+        image_name = self.lines[index].split(' ')[0] + '.jpg'
+        caption = ' '.join(self.lines[index].split(' ')[1:])
+        # pdb.set_trace()
+        image = Image.open(os.path.join(
+            root, 'Flicker8k_Dataset', image_name)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(
+            str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        return image, target, index, img_id
+
+    def __len__(self):
+        return len(self.lines)
+
+
+def collate_fn(data):
+
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions, ids, _ = zip(*data)
+
+    # Merge images (convert tuple of 3D tensor to 4D tensor)
+    images = torch.stack(images, 0)
+
+    # Merget captions (convert tuple of 1D tensor to 2D tensor)
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+
+    return images, targets, lengths, list(ids)
+
+
+class PrecompDataset(data.Dataset):
+    """
+    Load precomputed captions and image features
+    Possible options: f8k, f30k, coco, 10crop
+    """
+
+    def __init__(self, data_path, data_split, vocab):
+        self.vocab = vocab
+        loc = data_path + '/'
+
+        # Captions
+        self.captions = []
+        with open(loc+'f8k_%s_caps.txt' % data_split, 'rb') as f:
+            for line in f:
+                self.captions.append(line.strip())
+
+        # Image features
+        self.images = np.load(loc+'f8k_%s_ims.npy' % data_split)
+        self.length = len(self.captions)
+        # rkiros data has redundancy in images, we divide by 5, 10crop doesn't
+        if self.images.shape[0] != self.length:
+            self.im_div = 5
+        else:
+            self.im_div = 1
+        # the development set for coco is large and so validation would be slow
+        if data_split == 'dev':
+            self.length = 5000
+
+    def __getitem__(self, index):
+        # handle the image redundancy
+        img_id = index//self.im_div
+        image = torch.Tensor(self.images[img_id])
+        caption = self.captions[index]
+        vocab = self.vocab
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(
+            caption.lower().decode('utf-8'))
+        caption = []
+        # pdb.set_trace()
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        return image, target, index, img_id
+
+    def __len__(self):
+        return self.length
+
+
+def get_precomp_loader(data_path, data_split, vocab, batch_size=100,
+                       shuffle=True):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    dset = PrecompDataset(data_path, data_split, vocab)
+
+    data_loader = torch.utils.data.DataLoader(dataset=dset,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              pin_memory=True,
+                                              collate_fn=collate_fn)
+    return data_loader
+
+
+def get_transform(split_name):
+    normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                      std=[0.229, 0.224, 0.225])
+    t_list = []
+    if split_name == 'train':
+        t_list = [transforms.RandomResizedCrop(224),
+                  transforms.RandomHorizontalFlip()]
+    elif split_name == 'dev':
+        t_list = [transforms.Resize(256), transforms.CenterCrop(224)]
+    elif split_name == 'test':
+        t_list = [transforms.Resize(256), transforms.CenterCrop(224)]
+
+    t_end = [transforms.ToTensor(), normalizer]
+    transform = transforms.Compose(t_list + t_end)
+    return transform
+
+
+def get_loader_single(root, split, vocab, transform,
+                      batch_size=128, shuffle=True,
+                      collate_fn=collate_fn):
+
+    dataset = FlickrDataset(root=root,
+                            split=split,
+                            vocab=vocab,
+                            transform=transform)
+
+    # Data loader
+    data_loader = torch.utils.data.DataLoader(dataset=dataset,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              collate_fn=collate_fn)
+    return data_loader
+
+
+class AverageMeter:
+    def __init__(self, name):
+        self.name = name
+        self.count = 0
+        self.total = 0
+        self.max = -1*float("inf")
+        self.min = float("inf")
+
+    def add(self, element):
+        # pdb.set_trace()
+        self.total += element
+        self.count += 1
+        self.max = max(self.max, element)
+        self.min = min(self.min, element)
+
+    def compute(self):
+        # pdb.set_trace()
+        if self.count == 0:
+            return float("inf")
+        return self.total/self.count
+
+    def __str__(self):
+        return "%s (min, avg, max): (%.3lf, %.3lf, %.3lf)" % (self.name, self.min, self.compute(), self.max)
+
+
+def i2t(images, captions, npts=None):
+    """
+    Images->Text (Image Annotation)
+    Images: (5N, K) matrix of images
+    Captions: (5N, K) matrix of captions
+    """
+    index_list = []
+    if npts == None:
+        npts = images.shape[0] // 5
+
+    ranks = np.zeros(npts)
+    for index in range(npts):
+
+        # Get query image
+        im = images[5 * index].reshape(1, images.shape[1])
+
+        # Compute scores
+        d = np.dot(im, captions.T).flatten()
+        inds = np.argsort(d)[::-1]
+        index_list.append(inds[0])
+        # Score
+        rank = 1e20
+        # find the highest ranking
+        for i in range(5*index, 5*index + 5, 1):
+            tmp = np.where(inds == i)[0][0]
+            if tmp < rank:
+                rank = tmp
+        ranks[index] = rank
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    return (r1, r5, r10, medr)
+
+
+def t2i(images, captions, npts=None):
+    """
+    Text->Images (Image Search)
+    Images: (5N, K) matrix of images
+    Captions: (5N, K) matrix of captions
+    """
+    if npts == None:
+        npts = images.shape[0] // 5
+
+    ims = np.array([images[i] for i in range(0, len(images), 5)])
+
+    ranks = np.zeros(5 * npts)
+    for index in range(npts):
+
+        # Get query captions
+        queries = captions[5*index: 5*index + 5]
+
+        # Compute scores
+        d = np.dot(queries, ims.T)
+        inds = np.zeros(d.shape)
+        for i in range(len(inds)):
+            inds[i] = np.argsort(d[i])[::-1]
+            ranks[5 * index + i] = np.where(inds[i] == index)[0][0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    return (r1, r5, r10, medr)
+
+
+def encode_data(txt_enc, img_enc, data_loader):
+    img_embs = None
+    cap_embs = None
+    for i, (images, captions, lengths, ids) in enumerate(tqdm(data_loader)):
+        images = images.to(device)
+        captions = captions.to(device)
+        img_emb = img_enc(images)
+        cap_emb = txt_enc(captions, lengths)
+        if img_embs is None:
+            img_embs = np.zeros((len(data_loader.dataset), img_emb.size(1)))
+            cap_embs = np.zeros((len(data_loader.dataset), cap_emb.size(1)))
+
+        # preserve the embeddings by copying from gpu and converting to numpy
+        img_embs[ids] = img_emb.data.cpu().numpy().copy()
+        cap_embs[ids] = cap_emb.data.cpu().numpy().copy()
+    return img_embs, cap_embs
